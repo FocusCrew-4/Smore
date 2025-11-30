2e900d3ef3856d3e593cb0af2f95932fbb968afb
yoo20370 <yoo20370@gmail.com>
2025-11-30 16:38:49 +0900

    feat(Order) : 주문 Entity 정의 및 관련 객체 추가

---
+++ order/src/main/java/com/smore/order/infrastructure/persistence/entity/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.smore.order.infrastructure.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@FilterDef(
+    name = "softDeleteFilter",
+    defaultCondition = "deleted_at IS NULL",
+    autoEnabled = true,
+    applyToLoadByKey = true
+)
+@Filter(name = "softDeleteFilter")
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "deleted_by")
+    private Long deletedBy;
+}
