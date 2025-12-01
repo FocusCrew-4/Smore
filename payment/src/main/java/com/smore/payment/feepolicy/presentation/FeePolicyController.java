7f64323630fb015d178f077332830e8ce77e0a09
ooinl77 <ooinl77@naver.com>
2025-12-01 17:34:49 +0900

    refactor: auditing 설정 변경 및 적용

--- payment/src/main/java/com/smore/payment/feepolicy/presentation/FeePolicyController.java
+++ payment/src/main/java/com/smore/payment/feepolicy/presentation/FeePolicyController.java
@@ -6,6 +6,7 @@ import com.smore.payment.feepolicy.application.query.GetFeePolicyQuery;
 import com.smore.payment.feepolicy.domain.model.*;
 import com.smore.payment.feepolicy.presentation.dto.CreateFeePolicyRequestDto;
 import com.smore.payment.feepolicy.presentation.dto.GetFeePolicyRequest;
+import com.smore.payment.global.config.UserContextHolder;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -21,8 +22,11 @@ public class FeePolicyController {
     private final FeePolicyService feePolicyService;
 
     @PostMapping
-    public ResponseEntity<?> createFeePolicy(@Valid @RequestBody CreateFeePolicyRequestDto createFeePolicyDto) {
-
+    public ResponseEntity<?> createFeePolicy(
+            @Valid @RequestBody CreateFeePolicyRequestDto createFeePolicyDto,
+            @RequestHeader("X-USER-ID") UUID userId
+    ) {
+        UserContextHolder.set(userId);
         CreateFeePolicyCommand createFeePolicyCommand = new CreateFeePolicyCommand(
                 TargetType.valueOf(createFeePolicyDto.getTargetType()),
                 createFeePolicyDto.getTargetKey(),
@@ -33,6 +37,7 @@ public class FeePolicyController {
 
         UUID id = feePolicyService.createFeePolicy(createFeePolicyCommand);
 
+        UserContextHolder.clear();
         //Todo: 공통응답 사용해서 id 반환
         return ResponseEntity.ok().build();
     }
@@ -53,9 +58,9 @@ public class FeePolicyController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteFeePolicy(@PathVariable UUID id, @RequestHeader("X-USER-ID") UUID userId) {
-
+        UserContextHolder.set(userId);
         feePolicyService.deleteFeePolicy(id, userId);
-
+        UserContextHolder.clear();
         return ResponseEntity.ok().build();
     }
 }
