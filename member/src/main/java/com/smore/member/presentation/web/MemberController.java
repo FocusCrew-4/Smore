6b7bcbdc968b7de4a368516d1ef7c3e24fbee733
yawning5 <yawning055@gmail.com>
2025-12-01 11:53:48 +0900

    feat: login 코드 작성

--- member/src/main/java/com/smore/member/presentation/web/MemberController.java
+++ member/src/main/java/com/smore/member/presentation/web/MemberController.java
@@ -2,15 +2,18 @@ package com.smore.member.presentation.web;
 
 import com.smore.common.response.ApiResponse;
 import com.smore.common.response.CommonResponse;
+import com.smore.member.application.service.AuthService;
 import com.smore.member.application.service.RoleBasedMemberService;
 import com.smore.member.application.service.result.MemberResult;
 import com.smore.member.application.service.selector.MemberServiceSelector;
 import com.smore.member.domain.enums.Role;
 import com.smore.member.presentation.web.dto.request.CreateRequestDto;
+import com.smore.member.presentation.web.dto.request.LoginRequestDto;
 import com.smore.member.presentation.web.dto.response.CreateResponseDto;
 import com.smore.member.presentation.web.mapper.MemberControllerMapper;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +28,15 @@ public class MemberController {
 
     private final MemberServiceSelector memberServiceSelector;
     private final MemberControllerMapper mapper;
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponse<?>> login(@RequestBody LoginRequestDto requestDto) {
+        String token = authService.login(mapper.toLoginCommand(requestDto));
+        return ResponseEntity.ok()
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+            .body(ApiResponse.ok(null));
+    }
 
     @PostMapping
     public ResponseEntity<CommonResponse<CreateResponseDto>> registerConsumer(
--- member/src/main/java/com/smore/member/presentation/web/mapper/MemberControllerMapper.java
+++ member/src/main/java/com/smore/member/presentation/web/mapper/MemberControllerMapper.java
@@ -1,13 +1,22 @@
 package com.smore.member.presentation.web.mapper;
 
 import com.smore.member.application.service.command.CreateCommand;
+import com.smore.member.application.service.command.LoginCommand;
 import com.smore.member.application.service.result.MemberResult;
 import com.smore.member.presentation.web.dto.request.CreateRequestDto;
+import com.smore.member.presentation.web.dto.request.LoginRequestDto;
 import com.smore.member.presentation.web.dto.response.CreateResponseDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MemberControllerMapper {
+
+    public LoginCommand toLoginCommand(LoginRequestDto requestDto) {
+        return new LoginCommand(
+            requestDto.email(),
+            requestDto.password()
+        );
+    };
 
     public CreateCommand toCreateCommand(CreateRequestDto requestDto) {
         return new CreateCommand(
