24937e823ec8065b4c10195e86a54e3b2e457592
ooinl77 <ooinl77@naver.com>
2025-12-02 02:57:07 +0900

    feat: CancelPolicy persistence entiy

---
+++ payment/src/main/java/com/smore/payment/cancelpolicy/infrastructure/persistence/mapper/CancelPolicyMapper.java
@@ -0,0 +1,58 @@
+package com.smore.payment.cancelpolicy.infrastructure.persistence.mapper;
+
+import com.smore.payment.cancelpolicy.domain.model.CancelFeeRate;
+import com.smore.payment.cancelpolicy.domain.model.CancelFixedAmount;
+import com.smore.payment.cancelpolicy.domain.model.CancelPolicy;
+import com.smore.payment.cancelpolicy.infrastructure.persistence.model.CancelPolicyEntity;
+import com.smore.payment.cancelpolicy.infrastructure.persistence.model.CancelFeeRateJpa;
+import com.smore.payment.cancelpolicy.infrastructure.persistence.model.CancelFixedAmountJpa;
+import com.smore.payment.feepolicy.domain.model.FeePolicy;
+import com.smore.payment.feepolicy.domain.model.FeeRate;
+import com.smore.payment.feepolicy.domain.model.FixedAmount;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CancelPolicyMapper {
+
+    public CancelPolicyEntity toEntity(CancelPolicy cancelPolicy) {
+        CancelFeeRateJpa cancelFeeRateJpa = new CancelFeeRateJpa(
+                cancelPolicy.getCancelFeeRate().value()
+        );
+        CancelFixedAmountJpa cancelFixedAmountJpa = new CancelFixedAmountJpa(
+                cancelPolicy.getCancelFixedAmount().value()
+        );
+
+        return new CancelPolicyEntity(
+                cancelPolicy.getId(),
+                cancelPolicy.getCancelTargetType(),
+                cancelPolicy.getTargetKey(),
+                cancelPolicy.getCancelLimitMinutes(),
+                cancelPolicy.getCancelFeeType(),
+                cancelFeeRateJpa,
+                cancelFixedAmountJpa,
+                cancelPolicy.isCancellable(),
+                cancelPolicy.isActive()
+        );
+    }
+
+    public CancelPolicy toDomainEntity(CancelPolicyEntity cancelPolicyEntity) {
+        CancelFeeRate feeRate = new CancelFeeRate(
+                cancelPolicyEntity.getCancelFeeRateJpa().getRate()
+        );
+        CancelFixedAmount fixedAmount = new CancelFixedAmount(
+                cancelPolicyEntity.getCancelFixedAmountJpa().getFixedAmount()
+        );
+
+        return CancelPolicy.reconstruct(
+                cancelPolicyEntity.getId(),
+                cancelPolicyEntity.getCancelTargetType(),
+                cancelPolicyEntity.getTargetKey(),
+                cancelPolicyEntity.getCancelLimitMinutes(),
+                cancelPolicyEntity.getCancelFeeType(),
+                feeRate,
+                fixedAmount,
+                cancelPolicyEntity.isCancellable(),
+                cancelPolicyEntity.isActive()
+        );
+    }
+}
---
+++ payment/src/main/java/com/smore/payment/cancelpolicy/infrastructure/persistence/model/CancelPolicyEntity.java
@@ -0,0 +1,75 @@
+package com.smore.payment.cancelpolicy.infrastructure.persistence.model;
+
+import com.smore.payment.cancelpolicy.domain.model.CancelFeeType;
+import com.smore.payment.cancelpolicy.domain.model.CancelTargetType;
+import com.smore.payment.feepolicy.domain.model.FeeType;
+import com.smore.payment.feepolicy.domain.model.TargetType;
+import com.smore.payment.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "fee_policies")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class CancelPolicyEntity extends BaseEntity {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cancel_target_type", nullable = false, updatable = false)
+    private CancelTargetType cancelTargetType;
+
+    @Column(name = "target_key", nullable = false, updatable = false)
+    private UUID targetKey;
+
+    @Column(name = "cancel_limit_minutes")
+    private Integer cancelLimitMinutes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "cancel_fee_type", nullable = false, updatable = false)
+    private CancelFeeType cancelFeeType;
+
+    @Embedded
+    private CancelFeeRateJpa cancelFeeRateJpa;
+
+    @Embedded
+    private CancelFixedAmountJpa cancelFixedAmountJpa;
+
+    @Column(name = "cancellable", nullable = false, updatable = false)
+    private boolean cancellable;
+
+    @Setter
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    public CancelPolicyEntity(
+            UUID id,
+            CancelTargetType cancelTargetType,
+            UUID targetKey,
+            Integer cancelLimitMinutes,
+            CancelFeeType cancelFeeType,
+            CancelFeeRateJpa rate,
+            CancelFixedAmountJpa fixedAmount,
+            boolean cancellable,
+            boolean active
+    ) {
+        this.id = id;
+        this.cancelTargetType = cancelTargetType;
+        this.targetKey = targetKey;
+        this.cancelLimitMinutes = cancelLimitMinutes;
+        this.cancelFeeType = cancelFeeType;
+        this.cancelFeeRateJpa = rate;
+        this.cancelFixedAmountJpa = fixedAmount;
+        this.cancellable = cancellable;
+        this.active = active;
+    }
+
+}
