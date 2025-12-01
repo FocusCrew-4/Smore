6fb2cda1361927200e01247cf029fa5a3cd13c3a
ooinl77 <ooinl77@naver.com>
2025-12-01 10:45:34 +0900

    feat: FeePolicy Controller 기본 구조 생성

---
+++ payment/src/main/java/com/smore/payment/feepolicy/presentation/FeePolicyController.java
@@ -0,0 +1,39 @@
+package com.smore.payment.feepolicy.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/fee-policies")
+public class FeePolicyController {
+
+    private final FeePolicyService feePolicyService;
+
+    @PostMapping
+    public ResponseEntity<?> createFeePolicy(@RequestBody CreateFeePolicyRequestDto createFeePolicyDto) {
+
+        feePolicyService.createFeePolicy(createFeePolicyDto);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getFeePolicy(@PathVariable UUID id) {
+
+        feePolicyService.getFeePolicy(id);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteFeePolicy(@PathVariable UUID id) {
+
+        feePolicyService.deleteFeePolicy(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
