3fd09125fa1799ee6268d2880a47cac4a80e1a7d
ooinl77 <ooinl77@naver.com>
2025-12-03 04:57:57 +0900

    feat: CancelPolicy repository 구현

--- payment/src/main/java/com/smore/payment/cancelpolicy/domain/repository/CancelPolicyRepository.java
+++ payment/src/main/java/com/smore/payment/cancelpolicy/domain/repository/CancelPolicyRepository.java
@@ -1,7 +1,20 @@
 package com.smore.payment.cancelpolicy.domain.repository;
 
+import com.smore.payment.cancelpolicy.domain.model.CancelPolicy;
+import com.smore.payment.cancelpolicy.domain.model.CancelTargetType;
 import org.springframework.stereotype.Repository;
+
+import java.lang.ScopedValue;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface CancelPolicyRepository {
+    void save(CancelPolicy cancelPolicy);
+
+    Optional<CancelPolicy> findByTargetTypeAndTargetKey(CancelTargetType cancelTargetType, UUID uuid);
+
+    void delete(CancelPolicy cancelPolicy, UUID userId);
+
+    Optional<CancelPolicy> findById(UUID id);
 }
