4742ff1de214c71046cd9060109a20d8d66332ae
yoo20370 <yoo20370@gmail.com>
2025-12-03 01:14:06 +0900

    fix(Order) : @Value가 Bean이 아닌 클래스에서 실행 안 되는 문제 해결

--- order/src/main/java/com/smore/order/application/command/CreatedOrderCommand.java
+++ order/src/main/java/com/smore/order/application/command/CreatedOrderCommand.java
@@ -3,18 +3,17 @@ package com.smore.order.application.command;
 import com.smore.order.domain.model.Outbox;
 import com.smore.order.domain.status.OutboxResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Slf4j(topic = "CreatedOrderCommand")
 public class CreatedOrderCommand implements OutboxCommand {
 
-    @Value("${topic.order.created}")
-    private String topic;
+    private final String topic;
     private final KafkaTemplate<String, String> kafkaTemplate;
     private final Outbox outbox;
 
-    public CreatedOrderCommand(KafkaTemplate<String, String> kafkaTemplate, Outbox outbox) {
+    public CreatedOrderCommand(String topic, KafkaTemplate<String, String> kafkaTemplate, Outbox outbox) {
+        this.topic = topic;
         this.kafkaTemplate = kafkaTemplate;
         this.outbox = outbox;
     }
--- order/src/main/java/com/smore/order/application/factory/OutboxCommandFactory.java
+++ order/src/main/java/com/smore/order/application/factory/OutboxCommandFactory.java
@@ -1,9 +1,11 @@
 package com.smore.order.application.factory;
 
+import com.smore.order.application.command.CompletedOrderCommand;
 import com.smore.order.application.command.CreatedOrderCommand;
 import com.smore.order.application.command.OutboxCommand;
 import com.smore.order.domain.model.Outbox;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +15,16 @@ public class OutboxCommandFactory {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    @Value("${topic.order.created}")
+    private String orderCreatedTopic;
+
+    @Value("${topic.order.completed}")
+    private String orderCompletedTopic;
+
     public OutboxCommand from(Outbox outbox) {
         return switch (outbox.getEventType()) {
-            case ORDER_CREATED -> new CreatedOrderCommand(kafkaTemplate, outbox);
-            case ORDER_COMPLETED -> null;
+            case ORDER_CREATED -> new CreatedOrderCommand(orderCreatedTopic, kafkaTemplate, outbox);
+            case ORDER_COMPLETED -> new CompletedOrderCommand(orderCompletedTopic, kafkaTemplate, outbox);
             case ORDER_FAILED -> null;
             case PAYMENT_CANCEL -> null;
             case ORDER_CANCELLED -> null;
