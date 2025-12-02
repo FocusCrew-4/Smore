90e4c3180eb8649fe1e7617e23d3f8372169471e
An-jihyun <anjh322@gmail.com>
2025-12-02 12:40:56 +0900

    chore: common 문제 해결 전 임시 저장

--- product/src/main/java/com/smore/product/presentation/controller/ProductController.java
+++ product/src/main/java/com/smore/product/presentation/controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.smore.product.presentation.controller;
 
-import com.smore.common.response.ApiResponse;
-import com.smore.common.response.CommonResponse;
+//import com.smore.common.response.ApiResponse;
+//import com.smore.common.response.CommonResponse;
 import com.smore.product.presentation.dto.request.CreateProductRequest;
 import com.smore.product.presentation.dto.response.ProductResponse;
 import com.smore.product.application.service.ProductService;
@@ -18,14 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
 
-    @PostMapping
-    public ResponseEntity<CommonResponse<ProductResponse>> create(
-            @RequestBody CreateProductRequest request
-    ) {
-
-        ProductResponse response = productService.createProduct(request);
-
-        return ResponseEntity.ok()
-                .body(ApiResponse.ok(response));
-    }
+//    @PostMapping
+//    public ResponseEntity<CommonResponse<ProductResponse>> create(
+//            @RequestBody CreateProductRequest request
+//    ) {
+//
+//        ProductResponse response = productService.createProduct(request);
+//
+//        return ResponseEntity.ok()
+//                .body(ApiResponse.ok(response));
+//    }
 }
