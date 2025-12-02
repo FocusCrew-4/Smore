43615d98a438d4558bd898d07fa6e9c01c4bfa22
yawning5 <yawning055@gmail.com>
2025-12-02 16:04:18 +0900

    refactor: 기존 인터페이스들이 RoleSupportable 상속하도록 변경

--- member/src/main/java/com/smore/member/application/service/usecase/MemberInfoUpdate.java
+++ member/src/main/java/com/smore/member/application/service/usecase/MemberInfoUpdate.java
@@ -3,7 +3,7 @@ package com.smore.member.application.service.usecase;
 import com.smore.member.application.service.command.InfoUpdateCommand;
 import com.smore.member.application.service.result.MemberResult;
 
-public interface MemberInfoUpdate {
+public interface MemberInfoUpdate extends RoleSupportable {
 
     MemberResult update(InfoUpdateCommand command);
 
