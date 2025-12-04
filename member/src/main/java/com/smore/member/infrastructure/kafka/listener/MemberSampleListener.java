d9cbd9624736b0275ba97c77ff9bc4da17c8cdf9
yawning5 <yawning055@gmail.com>
2025-12-05 02:26:41 +0900

    feat: seller.register.v1 메시지 리스너 작성
    - 메시지 수신 확인
    - 수신 후 내부 기능 정상 작동 확인
    - DB 데이터 변경 확인

--- member/src/main/java/com/smore/member/infrastructure/kafka/listener/MemberSampleListener.java
+++ member/src/main/java/com/smore/member/infrastructure/kafka/listener/MemberSampleListener.java
@@ -1,19 +1,45 @@
 package com.smore.member.infrastructure.kafka.listener;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.smore.member.application.service.usecase.MemberRoleChange;
+import com.smore.member.infrastructure.kafka.listener.dto.SellerRegisterV1;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @ConditionalOnProperty(name = "spring.kafka.bootstrap-servers")
+@RequiredArgsConstructor
 public class MemberSampleListener {
 
+    private final MemberRoleChange memberRoleChange;
+    private final ObjectMapper objectMapper;
+
     @KafkaListener(
-        topics = "member-sample"
+        topics = "seller.register.v1"
     )
-    public void consume(String message) {
-        log.info("Received message on member-sample: {}", message);
+    public void consume(String event, Acknowledgment ack) {
+        try {
+            log.info("Received message on member-sample: {}", event);
+
+            // 처리 로직
+            // ...
+            SellerRegisterV1 sellerRegisterV1 = objectMapper.readValue(event, SellerRegisterV1.class);
+            log.info("SellerRegisterV1: {}", sellerRegisterV1);
+            log.info("SellerRegisterV1: {}", sellerRegisterV1.memberId());
+            log.info("SellerRegisterV1: {}", sellerRegisterV1.idempotencyKey());
+            memberRoleChange.toSeller(sellerRegisterV1.memberId());
+
+            ack.acknowledge(); // 수동 offset commit 필수
+        } catch (Exception e) {
+            log.error("Error processing message", e);
+            // ack 안 함 → 메시지 다시 받을 수 있음
+            // 또는 retry, DLQ 처리 전략 선택
+        }
+
     }
 }
