686bbd9a927df356d45ed9e5e2253175c06a0fc2
An-jihyun <anjh322@gmail.com>
2025-12-05 10:53:17 +0900

    ref: 도메인 주도 생성/수정 적용 및 JPA 콜백 제거

--- product/src/main/java/com/smore/product/application/service/ProductService.java
+++ product/src/main/java/com/smore/product/application/service/ProductService.java
@@ -7,6 +7,7 @@ import com.smore.product.domain.entity.SaleType;
 import com.smore.product.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    @Transactional
     public ProductResponse createProduct(CreateProductRequest req) {
 
         if (req.getSaleType() == SaleType.LIMITED_TO_AUCTION
@@ -22,16 +24,16 @@ public class ProductService {
             throw new IllegalArgumentException("LIMITED_TO_AUCTION requires thresholdForAuction");
         }
 
-        Product product = Product.builder()
-                .sellerId(req.getSellerId())
-                .categoryId(req.getCategoryId())
-                .name(req.getName())
-                .description(req.getDescription())
-                .price(req.getPrice())
-                .stock(req.getStock())
-                .saleType(req.getSaleType())
-                .thresholdForAuction(req.getThresholdForAuction())
-                .build();
+        Product product = Product.create(
+                req.getSellerId(),
+                req.getCategoryId(),
+                req.getName(),
+                req.getDescription(),
+                req.getPrice(),
+                req.getStock(),
+                req.getSaleType(),
+                req.getThresholdForAuction()
+        );
 
         productRepository.save(product);
 
@@ -39,9 +41,9 @@ public class ProductService {
     }
 
     public ProductResponse getProduct(UUID productId) {
-        Product p = productRepository.findById(productId)
+        Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("상품을 찾을 수 없습니다."));
 
-        return new ProductResponse(p);
+        return new ProductResponse(product);
     }
 }
