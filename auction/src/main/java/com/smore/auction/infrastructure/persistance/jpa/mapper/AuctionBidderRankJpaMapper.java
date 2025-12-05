0b182038c677c5ea9f55a0a670e45a72afbcd059
yawning5 <yawning055@gmail.com>
2025-12-06 01:06:15 +0900

    feat: Jpa <-> Domain 간 매퍼구성

---
+++ auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/mapper/AuctionBidderRankJpaMapper.java
@@ -0,0 +1,19 @@
+package com.smore.auction.infrastructure.persistance.jpa.mapper;
+
+import com.smore.auction.domain.model.AuctionBidderRank;
+import com.smore.auction.domain.vo.Bidder;
+import com.smore.auction.infrastructure.persistance.jpa.entity.AuctionBidderRankJpa;
+import com.smore.auction.infrastructure.persistance.jpa.vo.BidderEmbeddable;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface AuctionBidderRankJpaMapper {
+
+    AuctionBidderRankJpa toEntity(AuctionBidderRank auctionBidderRank);
+
+    AuctionBidderRank toDomain(AuctionBidderRankJpa bidderRankJpa);
+
+    BidderEmbeddable toEntity(Bidder bidder);
+
+    Bidder toDomain(BidderEmbeddable bidderEmbeddable);
+}
