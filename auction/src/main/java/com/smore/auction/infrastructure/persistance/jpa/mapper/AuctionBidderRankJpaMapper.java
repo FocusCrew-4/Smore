3430eb6093e009366bfd025c1e6ed062c1bf981e
yawning5 <yawning055@gmail.com>
2025-12-06 03:12:35 +0900

    fix: MapStruct 동작 수정

--- auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/mapper/AuctionBidderRankJpaMapper.java
+++ auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/mapper/AuctionBidderRankJpaMapper.java
@@ -5,12 +5,14 @@ import com.smore.auction.domain.vo.Bidder;
 import com.smore.auction.infrastructure.persistance.jpa.entity.AuctionBidderRankJpa;
 import com.smore.auction.infrastructure.persistance.jpa.vo.BidderEmbeddable;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AuctionBidderRankJpaMapper {
 
     AuctionBidderRankJpa toEntity(AuctionBidderRank auctionBidderRank);
 
+    @Mapping(target = "auctionId", source = "auction.id")
     AuctionBidderRank toDomain(AuctionBidderRankJpa bidderRankJpa);
 
     BidderEmbeddable toEntity(Bidder bidder);
