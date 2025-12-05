e5ac77b03cc5e4b8a99fb36cb4a1740ddc3e3fc2
yawning5 <yawning055@gmail.com>
2025-12-06 03:05:46 +0900

    feat: Sql 레포 인터페이스와 jpa repo, AuctionCreate 유즈케이스와 Application 계층 mapper 추가

--- auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/AuctionJpaRepository.java
+++ auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/AuctionJpaRepository.java
@@ -1,9 +1,11 @@
 package com.smore.auction.infrastructure.persistance.jpa;
 
-import com.smore.auction.domain.model.Auction;
+import com.smore.auction.infrastructure.persistance.jpa.entity.AuctionJpa;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionJpaRepository extends JpaRepository<Auction, UUID> {
+public interface AuctionJpaRepository extends JpaRepository<AuctionJpa, UUID> {
 
+    Optional<AuctionJpa> findByProductId(UUID productId);
 }
