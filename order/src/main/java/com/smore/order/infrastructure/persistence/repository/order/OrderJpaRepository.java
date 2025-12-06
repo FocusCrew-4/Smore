2c3adbc9ee4674af929f070e50e1d1d853ae14c7
yoo20370 <yoo20370@gmail.com>
2025-12-07 00:41:13 +0900

    feat(Order) : softDeleteFilter가 적용되지 않는 주문 조회 NativeQuery 작성

--- order/src/main/java/com/smore/order/infrastructure/persistence/repository/order/OrderJpaRepository.java
+++ order/src/main/java/com/smore/order/infrastructure/persistence/repository/order/OrderJpaRepository.java
@@ -3,8 +3,18 @@ package com.smore.order.infrastructure.persistence.repository.order;
 import com.smore.order.infrastructure.persistence.entity.order.OrderEntity;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OrderJpaRepository extends JpaRepository<OrderEntity, UUID>,
     OrderJpaRepositoryCustom {
 
+    @Query(
+    value ="""
+        SELECT * 
+        FROM p_order 
+        WHERE id = :id
+    """,
+    nativeQuery = true)
+    OrderEntity findByIdIncludingDeleted(@Param("id") UUID orderId);
 }
