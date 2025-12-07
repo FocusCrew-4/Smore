6b9c279bb39af722344b369065fe5ec7f78e802e
yawning5 <yawning055@gmail.com>
2025-12-07 16:51:51 +0900

    fix: Mapper 에서 null 값을 반환하는 문제해결

--- auction/src/main/java/com/smore/auction/application/service/Impl/AuctionCreateImpl.java
+++ auction/src/main/java/com/smore/auction/application/service/Impl/AuctionCreateImpl.java
@@ -1,14 +1,19 @@
 package com.smore.auction.application.service.Impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smore.auction.application.command.AuctionCreateCommand;
 import com.smore.auction.application.service.usecase.AuctionCreate;
 import com.smore.auction.application.sql.AuctionSqlRepository;
 import com.smore.auction.domain.model.Auction;
 import java.time.Clock;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -16,13 +21,17 @@ public class AuctionCreateImpl implements AuctionCreate {
 
     private final AuctionSqlRepository auctionSqlRepository;
     private final Clock clock;
+    private final ObjectMapper objectMapper;
 
     // TODO: 예외타입 변환 필요
     @Override
+    @SneakyThrows
     public Auction create(AuctionCreateCommand command) {
         if (auctionSqlRepository.findByProductId(command.productId()) != null) {
             throw new IllegalArgumentException("Auction already exists");
         }
+
+        log.info("Creating auction {}", objectMapper.writeValueAsString(command));
 
         var auction = Auction.create(
             command.productId(),
@@ -32,7 +41,11 @@ public class AuctionCreateImpl implements AuctionCreate {
             clock
         );
 
+        log.info("Created auction {}", objectMapper.writeValueAsString(auction));
+
         Auction savedAuction = auctionSqlRepository.save(auction);
+
+        log.info("Saved auction {}", objectMapper.writeValueAsString(savedAuction));
 
         return savedAuction;
     }
--- auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/AuctionBidderRankJpaRepository.java
+++ auction/src/main/java/com/smore/auction/infrastructure/persistance/jpa/AuctionBidderRankJpaRepository.java
@@ -1,9 +1,10 @@
 package com.smore.auction.infrastructure.persistance.jpa;
 
 import com.smore.auction.domain.model.AuctionBidderRank;
+import com.smore.auction.infrastructure.persistance.jpa.entity.AuctionBidderRankJpa;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AuctionBidderRankJpaRepository extends JpaRepository<AuctionBidderRank, UUID> {
+public interface AuctionBidderRankJpaRepository extends JpaRepository<AuctionBidderRankJpa, UUID> {
 
 }
