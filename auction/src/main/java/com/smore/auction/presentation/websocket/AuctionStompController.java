4be1c53749b6b11d6af4e9ce2e116b0856df31cd
yawning5 <yawning055@gmail.com>
2025-12-07 15:44:36 +0900

    feat: 웹소켓을 사용한 메시징 송수신 기능 추가

---
+++ auction/src/main/java/com/smore/auction/presentation/websocket/AuctionStompController.java
@@ -0,0 +1,30 @@
+package com.smore.auction.presentation.websocket;
+
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class AuctionStompController {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/auction/{auctionId}/bid")
+    public void handleBid(
+        @DestinationVariable Long auctionId,
+        Map<String, Object> map
+    ) {
+        log.info("Received a bid request for {}", auctionId);
+        log.info("Received a bid request for {}", map.get("text"));
+        simpMessagingTemplate.convertAndSend(
+            "/sub/auction/" + auctionId,
+            map
+        );
+    }
+}
