397e186184815b016b30b65c55cdc58ce054c3c5
yawning5 <yawning055@gmail.com>
2025-12-08 21:51:20 +0900

    feat: WS 핸드셰이크시 WS session 에 principal 저장

--- auction/src/main/java/com/smore/auction/infrastructure/websocket/config/WebSocketConfig.java
+++ auction/src/main/java/com/smore/auction/infrastructure/websocket/config/WebSocketConfig.java
@@ -1,7 +1,16 @@
 package com.smore.auction.infrastructure.websocket.config;
 
+import com.smore.auction.infrastructure.websocket.AuctionUserPrincipal;
+import com.smore.auction.infrastructure.websocket.interceptor.AuctionHandshakeInterceptor;
+import java.util.Objects;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -16,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // http://localhost:6600/ws-auction
         registry.addEndpoint("/ws-auction")
             .setAllowedOriginPatterns("*")
+            .addInterceptors(new AuctionHandshakeInterceptor())
             .withSockJS(); // 선택: SockJS fallback
     }
 
@@ -25,5 +35,32 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/sub");
         // 클라이언트가 서버로 보낼때 클라이언트 -> 서버
         registry.setApplicationDestinationPrefixes("/pub");
+    }
+
+    /*
+    new ChannelInterceptor 를 직접 넣은 이유
+    beforeHandshake 에서 넣어둔 attributes 읽어서 CONNECT 프레임에서 Principal 생성해서 accessor.setUser(...)로 등록
+    간단한 사용자 식별자 등록 로직으로 외부 서비스 의존성도 없고 복잡한 주입도 필요없어 new 로 직접 등록
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend( Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+                if (accessor.getCommand() == StompCommand.CONNECT) {
+                    // attributes 의 타입이 Map<String, Object> 이기 때문에 Long 을 넣어도 컴파일러는 그 값을 Long 으로 저장
+                    // 따라서 다시 꺼낼 때 반환 타입도 Object 이므로 Long 으로 사용하려면 명시적 다운캐스팅이 필수
+                    Long userId = (Long) Objects.requireNonNull(accessor.getSessionAttributes()).get("userId");
+                    String role = (String) Objects.requireNonNull(accessor.getSessionAttributes()).get("role");
+
+                    // Principal 등록
+                    accessor.setUser(new AuctionUserPrincipal(userId, role));
+                }
+
+                return message;
+            }
+        });
     }
 }
---
+++ auction/src/main/java/com/smore/auction/infrastructure/websocket/interceptor/AuctionHandshakeInterceptor.java
@@ -0,0 +1,49 @@
+package com.smore.auction.infrastructure.websocket.interceptor;
+
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+@Slf4j
+public class AuctionHandshakeInterceptor implements HandshakeInterceptor {
+
+    // TODO: 예외타입 통일 필요
+    @Override
+    public boolean beforeHandshake(
+        ServerHttpRequest request,
+        ServerHttpResponse response,
+        WebSocketHandler wsHandler,
+        Map<String, Object> attributes)
+        throws Exception {
+
+        var headers = request.getHeaders();
+        String optionalUserId = headers.getFirst("X-USER-ID");
+        if (optionalUserId == null) {
+            throw new Exception("X-USER-ID header is missing");
+        }
+        String optionalRole = headers.getFirst("X-USER-ROLE");
+        if (optionalRole == null) {
+            throw new Exception("X-USER-ROLE header is missing");
+        }
+
+        attributes.put("USER_ID", Long.valueOf(optionalUserId));
+        attributes.put("ROLE", optionalRole);
+        return true;
+    }
+
+    @Override
+    public void afterHandshake(
+        ServerHttpRequest request,
+        ServerHttpResponse response,
+        WebSocketHandler wsHandler,
+        Exception exception) {
+        if (exception != null) {
+            log.error("Handshake failed: {}", exception.getMessage());
+        } else {
+            log.info("Handshake succeeded for request {}", request.getURI());
+        }
+    }
+}
