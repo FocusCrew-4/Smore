e64491ba486c9a67e21c06330f3e3b06289afe3c
yoo20370 <yoo20370@gmail.com>
2025-12-09 22:21:18 +0900

    feat(Order) : outbox에 저장된 주문 실패 이벤트를 다루는 Handler를 정의하고 Factory클래스에 해당 Handler를 등록

--- order/src/main/java/com/smore/order/application/factory/OutboxHandlerFactory.java
+++ order/src/main/java/com/smore/order/application/factory/OutboxHandlerFactory.java
@@ -2,6 +2,7 @@ package com.smore.order.application.factory;
 
 import com.smore.order.application.command.CompletedOrderHandler;
 import com.smore.order.application.command.CreatedOrderHandler;
+import com.smore.order.application.command.FailedOrderHandler;
 import com.smore.order.application.command.OutboxHandler;
 import com.smore.order.application.command.RefundFailedHandler;
 import com.smore.order.application.command.RefundRequestHandler;
@@ -34,6 +35,9 @@ public class OutboxHandlerFactory {
     @Value("${topic.order.refund-fail}")
     private String refundFailTopic;
 
+    @Value("${topic.order.failed}")
+    private String orderFailedTopic;
+
     public OutboxHandler from(Outbox outbox) {
         return switch (outbox.getEventType()) {
             case ORDER_CREATED -> new CreatedOrderHandler(orderCreatedTopic, kafkaTemplate, outbox);
@@ -41,6 +45,7 @@ public class OutboxHandlerFactory {
             case REFUND_REQUEST -> new RefundRequestHandler(refundRequestTopic, kafkaTemplate, outbox);
             case REFUND_SUCCESS -> new RefundSuccessHandler(refundSuccessTopic, kafkaTemplate, outbox);
             case REFUND_FAIL -> new RefundFailedHandler(refundFailTopic, kafkaTemplate, outbox);
+            case ORDER_FAILED -> new FailedOrderHandler(orderFailedTopic, kafkaTemplate, outbox);
             default -> throw new IllegalArgumentException(
                 "지원되지 않은 이벤트입니다." + outbox.getEventType()
             );
