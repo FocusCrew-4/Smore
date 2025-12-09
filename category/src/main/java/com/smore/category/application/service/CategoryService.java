4b8045a0057ccb7b464e8df7f7a3f98d7fe00f53
An-jihyun <anjh322@gmail.com>
2025-12-10 05:37:06 +0900

    feat: 카테고리 생성 서비스 로직 구현

---
+++ category/src/main/java/com/smore/category/application/service/CategoryService.java
@@ -0,0 +1,32 @@
+package com.smore.category.application.service;
+
+import com.smore.category.domain.entity.Category;
+import com.smore.category.domain.repository.CategoryRepository;
+import com.smore.category.presentation.dto.request.CreateCategoryRequest;
+import com.smore.category.presentation.dto.resopnse.CategoryResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    @Transactional
+    public CategoryResponse create(CreateCategoryRequest request) {
+
+        if (categoryRepository.existsByName(request.getName())) {
+            throw new IllegalArgumentException("이미 존재하는 카테고리입니다.");
+        }
+
+        Category category = new Category(
+                request.getName(),
+                request.getDescription()
+        );
+
+        categoryRepository.save(category);
+        return new CategoryResponse(category);
+    }
+}
