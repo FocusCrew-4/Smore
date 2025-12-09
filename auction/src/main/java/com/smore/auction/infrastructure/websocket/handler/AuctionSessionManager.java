078cc37f4c26302c7efb34d393ef9cc29ac69f91
yawning5 <yawning055@gmail.com>
2025-12-09 18:05:41 +0900

    refactor: 용도에 맞는 클래스명으로 변경

--- auction/src/main/java/com/smore/auction/infrastructure/websocket/handler/AuctionSessionManager.java
+++ auction/src/main/java/com/smore/auction/infrastructure/websocket/handler/AuctionSessionManager.java
@@ -1,6 +1,6 @@
 package com.smore.auction.infrastructure.websocket.handler;
 
-public interface AuctionSubHandler {
+public interface AuctionSessionManager {
     void handleSubscribe(String sessionId, Long userId, String auctionId);
     void handleDisconnect(String sessionId);
 }
