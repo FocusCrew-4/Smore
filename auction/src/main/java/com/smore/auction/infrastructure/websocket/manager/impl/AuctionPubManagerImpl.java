40a52bd88fa985c8b712b5b362d89f8584914150
yawning5 <yawning055@gmail.com>
2025-12-10 15:24:50 +0900

    refactor: redis key 를 한곳에서 일관되게 관리

--- auction/src/main/java/com/smore/auction/infrastructure/websocket/manager/impl/AuctionPubManagerImpl.java
+++ auction/src/main/java/com/smore/auction/infrastructure/websocket/manager/impl/AuctionPubManagerImpl.java
@@ -1,5 +1,6 @@
 package com.smore.auction.infrastructure.websocket.manager.impl;
 
+import com.smore.auction.infrastructure.redis.RedisKeyFactory;
 import com.smore.auction.infrastructure.websocket.manager.AuctionPubManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,22 +13,7 @@ import org.springframework.stereotype.Component;
 public class AuctionPubManagerImpl implements AuctionPubManager {
 
     private final StringRedisTemplate redis;
-
-    // ws:auction:501 = {sessionA, sessionB, sessionC}
-    private String keyAuction_sessions(String auctionId) {
-        return "ws:auction:" + auctionId;
-    }
-
-    // ws:session:abc123:user = 10
-    private String keySession_user(String sessionId) {
-        return "ws:session:" + sessionId + ":user";
-    }
-
-    // ws:session:abc123:auction = 501, 202, 333
-    private String keySession_auctions(String sessionId) {
-        return "ws:session:" + sessionId + ":auction";
-    }
-
+    private final RedisKeyFactory key;
 
     @Override
     public void registerAndGetEntryOrder(String sessionId, String auctionId) {
@@ -39,13 +25,13 @@ public class AuctionPubManagerImpl implements AuctionPubManager {
         log.info("validate Send 진입\n\n\n");
         log.info(sessionId);
         log.info(auctionId);
-        log.info(keyAuction_sessions(auctionId));
+        log.info(key.auctionSessions(auctionId));
         log.info(String.valueOf(redis.opsForSet()
-            .members(keyAuction_sessions(auctionId))));
+            .members(key.auctionSessions(auctionId))));
         log.info(String.valueOf(redis.opsForSet()
-            .isMember(keyAuction_sessions(auctionId), sessionId)));
+            .isMember(key.auctionSessions(auctionId), sessionId)));
         Boolean exists = redis.opsForSet()
-            .isMember(keyAuction_sessions(auctionId), sessionId);
+            .isMember(key.auctionSessions(auctionId), sessionId);
 
         if (exists == null || !exists) {
             throw new IllegalAccessException("세션이 해당 경매방에 참여 중이 아닙니다");
