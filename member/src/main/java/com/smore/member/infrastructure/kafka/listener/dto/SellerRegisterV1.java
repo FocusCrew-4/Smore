042fdf4ccfe363071184f805b34c6e185157fca2
yawning5 <yawning055@gmail.com>
2025-12-12 01:13:37 +0900

    fix: 이벤트 localdatetime 으로 타입변환 및 더미데이터 또한 localdatetime으로 수정

--- member/src/main/java/com/smore/member/infrastructure/kafka/listener/dto/SellerRegisterV1.java
+++ member/src/main/java/com/smore/member/infrastructure/kafka/listener/dto/SellerRegisterV1.java
@@ -1,12 +1,13 @@
 package com.smore.member.infrastructure.kafka.listener.dto;
 
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public record SellerRegisterV1(
     Long memberId,
     UUID idempotencyKey,
-    OffsetDateTime createdAt
+    LocalDateTime createdAt
 ) {
 
 }
