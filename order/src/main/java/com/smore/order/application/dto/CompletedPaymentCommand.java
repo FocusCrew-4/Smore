1681295cbbc23303a4144868512b80f23f8e7be1
yoo20370 <yoo20370@gmail.com>
2025-12-11 18:23:25 +0900

    fix(Order) : Order -> Bid로 가는 OrderCompletedEvent의 publishedAt을 PaidAt으로 수정

--- order/src/main/java/com/smore/order/application/dto/CompletedPaymentCommand.java
+++ order/src/main/java/com/smore/order/application/dto/CompletedPaymentCommand.java
@@ -1,5 +1,6 @@
 package com.smore.order.application.dto;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,16 +14,19 @@ public class CompletedPaymentCommand {
     private UUID orderId;
     private UUID paymentId;
     private Integer totalAmount;
+    private LocalDateTime approvedAt;
 
     public static CompletedPaymentCommand of(
         UUID orderId,
         UUID paymentId,
-        Integer totalAmount
+        Integer totalAmount,
+        LocalDateTime approvedAt
     ) {
         return CompletedPaymentCommand.builder()
             .orderId(orderId)
             .paymentId(paymentId)
             .totalAmount(totalAmount)
+            .approvedAt(approvedAt)
             .build();
     }
 }
