4211514f95e24a2423bd9e1ba4fcfc0e30ab72b8
yoo20370 <yoo20370@gmail.com>
2025-12-15 11:22:15 +0900

    feat(Bid) : 환불 성공했을 때, Bid가 이미 End 상태라면 product로 이벤트를 발행하도록 refundSuccess() 서비스 로직 수정

--- bidcompetition/src/main/java/com/smore/bidcompetition/domain/status/AggregateType.java
+++ bidcompetition/src/main/java/com/smore/bidcompetition/domain/status/AggregateType.java
@@ -4,7 +4,8 @@ public enum AggregateType {
     BID("판매 경쟁 이벤트"),
     AUCTION("경매 이벤트"),
     ORDER("주문 이벤트"),
-    PAYMENT("결제 이벤트")
+    PAYMENT("결제 이벤트"),
+    PRODUCT("상품 이벤트")
     ;
 
     private final String description;
--- bidcompetition/src/main/java/com/smore/bidcompetition/domain/status/EventType.java
+++ bidcompetition/src/main/java/com/smore/bidcompetition/domain/status/EventType.java
@@ -1,7 +1,8 @@
 package com.smore.bidcompetition.domain.status;
 
 public enum EventType {
-    BID_WINNER_SELECTED("경쟁 승리자 선정")
+    BID_WINNER_SELECTED("경쟁 승리자 선정"),
+    PRODUCT_INVENTORY_ADJUSTED("환불")
     ;
 
     private final String description;
