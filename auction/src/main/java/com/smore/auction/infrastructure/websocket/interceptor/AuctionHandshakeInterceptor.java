2211594a396af66ca0ed9d709c51eaff3c6d7464
yawning5 <yawning055@gmail.com>
2025-12-18 13:12:42 +0900

    feat: gateway 에서 유저 인증정보를 받아오도록 수정

--- auction/src/main/java/com/smore/auction/infrastructure/websocket/interceptor/AuctionHandshakeInterceptor.java
+++ auction/src/main/java/com/smore/auction/infrastructure/websocket/interceptor/AuctionHandshakeInterceptor.java
@@ -23,27 +23,26 @@ public class AuctionHandshakeInterceptor implements HandshakeInterceptor {
 
         // 웹소켓은 클라이언트 서버간 커스텀 헤더가 적용이 안 되고 서버간만 된다
         // TODO: gw 를 경유하므로 gw 에서 ws 업그레이드 요청 request 를 잡아서 header 값 추가해야함 gw 에서 쿼리파라미터로 jwt 받아와야함
-//        var headers = request.getHeaders();
-//        String optionalUserId = headers.getFirst("X-USER-ID");
-//        if (optionalUserId == null) {
-//            throw new Exception("X-USER-ID header is missing");
-//        }
-//        String optionalRole = headers.getFirst("X-USER-ROLE");
-//        if (optionalRole == null) {
-//            throw new Exception("X-USER-ROLE header is missing");
-//        }
-        // region socket 정상작동 확인시 활성화 필수
-//        if (!optionalRole.equalsIgnoreCase("customer")) {
-//            throw new Exception("Only customer roles are supported");
-//        }
-        // endregion
-
-//        attributes.put("USER_ID", Long.valueOf(optionalUserId));
-//        attributes.put("ROLE", optionalRole);
+        var headers = request.getHeaders();
+        String optionalUserId = headers.getFirst("X-USER-ID");
+        if (optionalUserId == null) {
+            throw new Exception("X-USER-ID header is missing");
+        }
+        String optionalRole = headers.getFirst("X-USER-ROLE");
+//        log.info("role 값 머들어온거임: {}", optionalRole);
+        if (optionalRole == null) {
+            throw new Exception("X-USER-ROLE header is missing");
+        }
+        if (!optionalRole.equalsIgnoreCase("consumer")) {
+            throw new Exception("Only customer roles are supported");
+        }
+        attributes.put("userId", Long.valueOf(optionalUserId));
+        attributes.put("role", optionalRole);
 
         log.info("핸드셰이크 진입\n\n\n\n\n");
-        attributes.put("userId", 1L);
-        attributes.put("role", "CUSTOMER");
+        log.debug("attributes: {}, {}", attributes.get("USER_ID"), attributes.get("ROLE"));
+//        attributes.put("userId", 1L);
+//        attributes.put("role", "CUSTOMER");
         return true;
     }
 
