cb6822eb84dae9b529a52e5501ac364a7107ae68
yoo20370 <yoo20370@gmail.com>
2025-12-29 23:36:12 +0900

    fix(Bid) : processing 상태 영구 고착 문제 해결 (만료 회수 스케줄러 추가)

--- bidcompetition/src/main/java/com/smore/bidcompetition/application/repository/OutboxRepository.java
+++ bidcompetition/src/main/java/com/smore/bidcompetition/application/repository/OutboxRepository.java
@@ -2,7 +2,9 @@ package com.smore.bidcompetition.application.repository;
 
 import com.smore.bidcompetition.domain.status.EventStatus;
 import com.smore.bidcompetition.domain.model.Outbox;
+import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,6 +13,8 @@ public interface OutboxRepository {
     Outbox findById(Long outboxId);
 
     Page<Long> findPendingIds(Collection<EventStatus> states, Pageable pageable);
+
+    List<Long> findExpiredProcessingIds(LocalDateTime expiredAt);
 
     Outbox save(Outbox outbox);
 
@@ -21,5 +25,7 @@ public interface OutboxRepository {
     int markRetry(Long outboxId, EventStatus eventStatus);
 
     int markFail(Long outboxId, EventStatus eventStatus, Integer maxRetryCount);
+
+    int bulkResetExpiredProcessingToReady(List<Long> ids);
 
 }
